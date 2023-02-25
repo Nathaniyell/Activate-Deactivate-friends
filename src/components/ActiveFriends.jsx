6e4cebef356a0b3f2938fd907ed244bb679134ba
Essien Nathaniel <thaniessy@gmail.com>
2023-02-25 04:04:04 +0000

    Installed propTypes to help with handling props, made updates to the inActiveFriends component

--- src/components/ActiveFriends.jsx
+++ src/components/ActiveFriends.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
 
 
 const ActiveFriends = (props)=>{
-    const friendList = props.activeList.map((friend, index)=>{
+      const friendList = props.activeList.map((friend, index)=>{
         return (
        <>
           <li key={index}>{friend}</li>
          <button onClick={()=> props.deactivate(friend)}>Deactivate</button>
        </>    
-        )
-     })
+        );
+     });
  
  
      return(
@@ -17,7 +18,11 @@ const ActiveFriends = (props)=>{
              <h1>Active Friends</h1>
             <ul>{friendList}</ul> 
              </>
-     )
-}
+     );
+};
+// ActiveFriends.propTypes ={
+//   activeList: PropTypes.arrayOf(PropTypes.string).isRequired,
+//   deactivate: PropTypes.func.isRequired,
+// };
 
-export default ActiveFriends
+export default ActiveFriends;
--- src/components/InactiveFriends.jsx
+++ src/components/InactiveFriends.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 
 
-const ActiveFriends = (props)=>{
-    const friendList = props.activeList.map((friend, index)=>{
-        return (
-       <>
-          <li key={index}>{friend}</li>
-         <button onClick={()=> props.deactivate(friend)}>Deactivate</button>
-       </>    
-        )
-     })
- 
- 
-     return(
-             <>
-             <h1>Active Friends</h1>
-            <ul>{friendList}</ul> 
-             </>
-     )
+const InactiveFriends = (props)=>{
+        const friendList = props.newFriend.map((friend, index)=>{
+       return (
+      <>
+         <li key={index}>{friend}</li>
+        <button onClick={()=>props.activate(friend)}>Activate</button>
+      </>    
+       )
+    })
+
+
+    return(
+            <>
+            <h1>Inactive Friends</h1>
+           <ul>{friendList}</ul> 
+            </>
+    )
 }
 
-export default ActiveFriends
+export default InactiveFriends
